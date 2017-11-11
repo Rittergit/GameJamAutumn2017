e7762a02c02fdea479f8caf8f362a9ac8a829c39
Daniel Martin <daniel.martin@hicknhack-software.com>
2017-11-11 16:40:14 +0100

    * Added moving objects.

--- GameJam2/Assets/Scripts/LevelBehavior.cs
+++ GameJam2/Assets/Scripts/LevelBehavior.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class LevelBehavior : MonoBehaviour {
 
-	// Use this for initialization
+	public Transform block;
+	private List<Transform> blocks;
+
 	void Start () {
-		
+		blocks = new List<Transform> ();
+
+		blocks.Add (Instantiate (block, new Vector3(0, 0, 120), Quaternion.identity));
+		blocks [0].Rotate (new Vector3 (0, 0, Random.Range (0, 5) * 60));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate () {
+		//blocks [0].transform.position -= new Vector3 (0, 0, 1);
+		if (Input.GetKeyDown ("up")) {
+			// Destroy GameObject
+			if (blocks.Count > 0) {
+				Destroy (blocks [0].gameObject);
+				blocks.RemoveAt (0);
+			}
+		}
 	}
 }
