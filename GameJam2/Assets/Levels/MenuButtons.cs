0018663a9dfa82aac72fdc3a7b8b2f31fcf457b5
shintobi <tobi_456@web.de>
2017-11-12 12:55:05 +0100

    * added menu

---
+++ GameJam2/Assets/Levels/MenuButtons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour {
+
+    public Transform camera;
+
+
+    public void PlayGame() {
+           
+            Application.LoadLevel(1);
+    
+    }
+
+
+    public void Settings()
+    {
+        camera.GetComponent<Animator>().SetBool("switch", true);
+
+        
+
+    }
+
+    public void TransMenu()
+    {
+        camera.GetComponent<Animator>().SetBool("switch", false);
+    }
+
+
+    public void Menu()
+    {
+
+        Application.LoadLevel(0);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
